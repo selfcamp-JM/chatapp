8a8076fcaa01e9dd1abe54840fba535ba5faa604
selfcamp-JM <jakhongir.mamadov@gmail.com>
2019-01-22 01:57:54 +0300

    First Commit

---
+++ src/server/index.js
@@ -0,0 +1,13 @@
+// Didnt understand so need to search after project is finished
+
+var app = require("http").createServer()
+var io = module.exports.io = require("socket.io")(app)
+
+const PORT = process.env.PORT || 3231
+
+const SocketManager = require("./SocketManager")
+
+io.connection("connection", SocketManager)
+app.listen(PORT, ()=>{
+    console.log("Connected to Port "+ PORT);
+})
